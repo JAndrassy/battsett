1ea660206c0840637428697391cdec8246ff7839
Duro <Duro@TV-PC>
2017-07-09 07:40:11 +0200

    repo link

--- src/battsett/BattSett.java
+++ src/battsett/BattSett.java
@@ -1,5 +1,6 @@
 /*
 Copyright (C) 2017 Juraj Andrássy
+repository https://github.com/jandrassy/battsett
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published
